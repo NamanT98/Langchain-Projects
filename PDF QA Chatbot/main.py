2518f6c59ff563502d23b775833dfa12eca5f405
NamanT98 <namant@iitbhilai.ac.in>
2024-08-19 13:56:31 +0530

    updated pdf chatbot

--- PDF QA Chatbot/main.py
+++ PDF QA Chatbot/main.py
@@ -1,25 +1,34 @@
 import streamlit as st
 from helper import *
 
-st.title("LangChain")
-user_input=st.text_input("Ask questions about pdf")
-
+st.title("PDF QA Chatbot")
 if 'submit' not in st.session_state:
     st.session_state.submit=False
+if 'chatbot' not in st.session_state:
+    st.session_state.chatbot=PdfChatbot()
+if 'messages' not in st.session_state:
+    st.session_state.messages=[]
+
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+user_input= st.chat_input("Ask questions about Youtube Video")
 
 with st.sidebar:
-# if not st.session_state.file:
     pdf_docs=st.file_uploader("Upload your pdf files !",accept_multiple_files=True)
     if st.button("Submit"):
         st.session_state.submit=True
         with st.spinner("Processing..."):
-            st.session_state.db=load_document(pdf_docs)
-            st.session_state.new_chain=get_qa_chain()
+            st.session_state.chatbot.create_db(pdf_docs)
             st.success("Done")
 
 if not st.session_state.submit:
     st.write("Please upload your pdf files")
 
 if user_input and st.session_state.submit:
-    response=run_qa(user_input,st.session_state.db,st.session_state.new_chain)
-    st.write(response)
+    st.chat_message("user").markdown(user_input)
+    st.session_state.messages.append({"role":"user","content":user_input})
+    response=st.session_state.chatbot.run_chain(user_input)
+    st.session_state.messages.append({"role":"assistant","content":response})
+    st.chat_message("assistant").markdown(response)
