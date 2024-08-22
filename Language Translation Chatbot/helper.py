e4643a10b26255160bd01d73cc4580283bd2cf22
NamanT98 <namant@iitbhilai.ac.in>
2024-08-22 23:44:22 +0530

    added translation chatbot template

---
+++ Language Translation Chatbot/helper.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.document_loaders import YoutubeLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
+from langchain.schema import HumanMessage,SystemMessage,AIMessage
+
+class ChatBot:
+    def __init__(self) -> None:
+        self.history =[SystemMessage(content="""
+        You are a helpful assistant that that can answer questions about youtube videos
+        based on the video's transcript. 
+        Only use the factual information from the transcript to answer the question.
+
+        Sometimes the query may not be related to the video's transcript and in such cases you are allowed to 
+        answer the questions on your own knowledge. 
+                                     
+        Your answers should be verbose and detailed.
+        """)]
+
+        self.llm=ChatGoogleGenerativeAI(model='gemini-1.5-pro-latest',
+                            temperature=0.7,
+                            top_p=0.85,
+                            )
+
+    def create_prompt(self,query):
+        prompt=f"""Using the transcript given below, answer the query.:
+        
+        Transcript:{self.get_context(query)}
+
+        Query:{query}"""
+
+        return prompt
+
+    
+    def create_db(self,url):
+        url=url.split('?v=')[1]
+        loader=YoutubeLoader(url,language=['en','en-US'])
+        transcript=loader.load()
+        text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        docs=text_splitter.split_documents(transcript)
+        embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+        self.db=FAISS.from_documents(docs,embeddings)
+        return self.db
+        
+    def get_context(self,query):
+        docs=self.db.similarity_search_with_score(query,8)
+        docs_page_content=" ".join([d[0].page_content for d in docs])
+        return docs_page_content
+
+    def run_chain(self,query):
+        self.history.append(HumanMessage(content=self.create_prompt(query)))
+        response=self.llm.invoke(self.history)
+        self.history.append(AIMessage(content=response.content))
+        return response.content
---
+++ Language Translation Chatbot/main.py
@@ -0,0 +1,37 @@
+from helper import *
+import streamlit as st
+
+st.title("Youtube video QA Chatbot")
+if 'submit' not in st.session_state:
+    st.session_state.submit=False
+if 'chatbot' not in st.session_state:
+    st.session_state.chatbot=ChatBot()
+if 'messages' not in st.session_state:
+    st.session_state.messages=[]
+
+# st.session_state.messages.append({"role":"assistant","content":"Hello👋 I am a Youtube video Question Answering bot. To start, paste the youtube video link and press submit"})
+
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+user_input= st.chat_input("Ask questions about Youtube Video")
+
+with st.sidebar:
+    url=st.text_input("Upload Youtube URL !")
+    if st.button("Submit"):
+        st.session_state.submit=True
+        with st.spinner("Processing..."):
+            st.session_state.chatbot.create_db(url)
+            st.success("Done")
+
+if not st.session_state.submit:
+    st.write("Please upload URL !")
+
+if user_input and st.session_state.submit:
+    st.chat_message("user").markdown(user_input)
+    st.session_state.messages.append({"role":"user","content":user_input})
+    response=st.session_state.chatbot.run_chain(user_input)
+    st.session_state.messages.append({"role":"assistant","content":response})
+    st.chat_message("assistant").markdown(response)
+    # st.write(response)
