e31eef327cc923b5d6c4d7ac1759e8c9554884ef
NamanT98 <namant@iitbhilai.ac.in>
2024-08-24 14:33:12 +0530

    completed translation app

--- Language Translation Chatbot/helper.py
+++ Language Translation Chatbot/helper.py
@@ -1,57 +1,38 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_community.document_loaders import YoutubeLoader
-from langchain_community.vectorstores import FAISS
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import HumanMessage,SystemMessage
 
-class ChatBot:
+class Translator:
     def __init__(self) -> None:
-        self.history =[SystemMessage(content="""
-        You are a helpful assistant that that can answer questions about youtube videos
-        based on the video's transcript. 
-        Only use the factual information from the transcript to answer the question.
+        self.system =SystemMessage(content="""
+        You are a language translation assistant. When a user provides a text and specifies the Source language,
+        your task is to verify that the text matches the specified source language.
+        If it does, accurately translate the text into the target language provided by the user.
+        If the text does not match the specified source language, inform the user of the discrepancy.
 
-        Sometimes the query may not be related to the video's transcript and in such cases you are allowed to 
-        answer the questions on your own knowledge. 
-                                     
-        Your answers should be verbose and detailed.
-        """)]
+        If you cannot translate the text into the target language then reply that you don't support the target language.
+        There should not be empty response.
+        """)
 
         self.llm=ChatGoogleGenerativeAI(model='gemini-1.5-pro-latest',
                             temperature=0.7,
                             top_p=0.85,
                             )
 
-    def create_prompt(self,query):
-        prompt=f"""Using the transcript given below, answer the query.:
+    def create_prompt(self,query,lang):
+        prompt=f"""Translate the below text from the given source and target language:
         
-        Transcript:{self.get_context(query)}
+        Text:{query}
 
-        Query:{query}"""
+        Source Language:{lang[0]}
+        Target Language:{lang[1]}"""
 
         return prompt
 
-    
-    def create_db(self,url):
-        url=url.split('?v=')[1]
-        loader=YoutubeLoader(url,language=['en','en-US'])
-        transcript=loader.load()
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-        docs=text_splitter.split_documents(transcript)
-        embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-        self.db=FAISS.from_documents(docs,embeddings)
-        return self.db
-        
-    def get_context(self,query):
-        docs=self.db.similarity_search_with_score(query,8)
-        docs_page_content=" ".join([d[0].page_content for d in docs])
-        return docs_page_content
-
-    def run_chain(self,query):
-        self.history.append(HumanMessage(content=self.create_prompt(query)))
-        response=self.llm.invoke(self.history)
-        self.history.append(AIMessage(content=response.content))
+    def run_chain(self,query,lang):
+        print(lang)
+        response=self.llm.invoke([self.system,HumanMessage(content=self.create_prompt(query,lang))])
+        # print("Response is:",response.content)
         return response.content
--- Language Translation Chatbot/main.py
+++ Language Translation Chatbot/main.py
@@ -1,37 +1,17 @@
 from helper import *
 import streamlit as st
 
-st.title("Youtube video QA Chatbot")
-if 'submit' not in st.session_state:
-    st.session_state.submit=False
+st.title("Language Translation")
 if 'chatbot' not in st.session_state:
-    st.session_state.chatbot=ChatBot()
-if 'messages' not in st.session_state:
-    st.session_state.messages=[]
-
-# st.session_state.messages.append({"role":"assistant","content":"Hello👋 I am a Youtube video Question Answering bot. To start, paste the youtube video link and press submit"})
-
-for message in st.session_state.messages:
-    with st.chat_message(message['role']):
-        st.markdown(message['content'])
-
-user_input= st.chat_input("Ask questions about Youtube Video")
+    st.session_state.chatbot=Translator()
 
 with st.sidebar:
-    url=st.text_input("Upload Youtube URL !")
-    if st.button("Submit"):
-        st.session_state.submit=True
-        with st.spinner("Processing..."):
-            st.session_state.chatbot.create_db(url)
-            st.success("Done")
+    lang1=st.text_input("Source language")
+    lang2=st.text_input("Target language")
 
-if not st.session_state.submit:
-    st.write("Please upload URL !")
+st.markdown("Paste Text here...")
+text= st.text_area(label='text area',height=400,label_visibility='hidden')
 
-if user_input and st.session_state.submit:
-    st.chat_message("user").markdown(user_input)
-    st.session_state.messages.append({"role":"user","content":user_input})
-    response=st.session_state.chatbot.run_chain(user_input)
-    st.session_state.messages.append({"role":"assistant","content":response})
-    st.chat_message("assistant").markdown(response)
-    # st.write(response)
+if text and lang1 and lang2:
+    response=st.session_state.chatbot.run_chain(text,(lang1,lang2))
+    st.write("\n"+response)
